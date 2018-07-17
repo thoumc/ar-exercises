2bda00839efbc4284f91cd7256f82ac65ef7ebbd
thoumc <thoumc@hotmail.com>
2018-07-17 16:22:30 +0000

    exercise 1 done

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+@store1 = Store.find_by (id: 1)
+@store1.name = 'Changed'
+@store1.save
+@store2 = Store.find_by (id: 2)
+
+
+puts @store1
 # Your code goes here ...
