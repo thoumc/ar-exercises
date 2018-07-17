98fc224ccffe7e8fe734ca5c6ee69dafd25606ee
thoumc <thoumc@hotmail.com>
2018-07-17 21:08:37 +0000

    exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -7,7 +7,8 @@ class Store < ActiveRecord::Base
 
   def has_department?
     if mens_apparel == false && womens_apparel == false
-      errors.add(:womens_apparel, "Needs a department")
+      errors.add(:womens_apparel, "Needs women department")
+      errors.add(:mens_apparel, "Needs men department")
     end
   end
 
